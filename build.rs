191ad8c6bd6f90deddfcd6de8e5eaf40539b7e75
seancarroll <seanc28@gmail.com>
2020-10-07 19:50:33 -0500

    copy templates to debug directory as part of build

---
+++ build.rs
@@ -0,0 +1,63 @@
+use std::{env, fs};
+use std::path::{Path, PathBuf};
+
+const TEMPLATES_DIR: &str = "templates";
+fn main() {
+    let target_dir_path = env::var("OUT_DIR").unwrap();
+
+    // target directory is /target/debug/build/doctavious-cli-.../out
+    // so we need to move it up into the target/debug directory
+    copy(TEMPLATES_DIR, Path::new(&target_dir_path).join("../../..").join(TEMPLATES_DIR)).unwrap();
+}
+
+fn copy<U: AsRef<Path>, V: AsRef<Path>>(from: U, to: V) -> Result<(), std::io::Error> {
+    let mut stack = Vec::new();
+    stack.push(PathBuf::from(from.as_ref()));
+
+    let output_root = PathBuf::from(to.as_ref());
+    let input_root = PathBuf::from(from.as_ref()).components().count();
+
+    while let Some(working_path) = stack.pop() {
+        println!("process: {:?}", &working_path);
+
+        // Generate a relative path
+        let src: PathBuf = working_path.components().skip(input_root).collect();
+
+        println!("src: {:?}", src.to_path_buf());
+
+        // Create a destination if missing
+        let dest = if src.components().count() == 0 {
+            output_root.clone()
+        } else {
+            output_root.join(&src)
+        };
+        if fs::metadata(&dest).is_err() {
+            println!(" mkdir: {:?}", dest);
+            fs::create_dir_all(&dest)?;
+        }
+
+        for entry in fs::read_dir(working_path)? {
+            let entry = entry?;
+            let path = entry.path();
+
+            println!(" found path: {:?}", path);
+
+            if path.is_dir() {
+                stack.push(path);
+            } else {
+                match path.file_name() {
+                    Some(filename) => {
+                        let dest_path = dest.join(filename);
+                        println!("  copy: {:?} -> {:?}", &path, &dest_path);
+                        fs::copy(&path, &dest_path)?;
+                    }
+                    None => {
+                        println!("failed: {:?}", path);
+                    }
+                }
+            }
+        }
+    }
+
+    Ok(())
+}
