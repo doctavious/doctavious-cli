bbdf9c30deb69cbde5fc68d73894c63a82c4a488
seancarroll <seanc28@gmail.com>
2020-10-10 20:40:50 -0500

    simplify build.rs by using walkdir

--- build.rs
+++ build.rs
@@ -1,64 +1,31 @@
 use std::{env, fs};
 use std::path::{Path, PathBuf};
 
+use walkdir::WalkDir;
+
 // TODO: move into src/main?
 const TEMPLATES_DIR: &str = "templates";
 fn main() {
-    let target_dir_path = env::var("OUT_DIR").unwrap();
-
-    // target directory is /target/debug/build/doctavious-cli-.../out
-    // so we need to move it up into the target/debug directory
-    copy(TEMPLATES_DIR, Path::new(&target_dir_path).join("../../..").join(TEMPLATES_DIR)).unwrap();
-}
-
-fn copy<U: AsRef<Path>, V: AsRef<Path>>(from: U, to: V) -> Result<(), std::io::Error> {
-    let mut stack = Vec::new();
-    stack.push(PathBuf::from(from.as_ref()));
-
-    let output_root = PathBuf::from(to.as_ref());
-    let input_root = PathBuf::from(from.as_ref()).components().count();
-
-    while let Some(working_path) = stack.pop() {
-        println!("process: {:?}", &working_path);
-
-        // Generate a relative path
-        let src: PathBuf = working_path.components().skip(input_root).collect();
-
-        println!("src: {:?}", src.to_path_buf());
-
-        // Create a destination if missing
-        let dest = if src.components().count() == 0 {
-            output_root.clone()
-        } else {
-            output_root.join(&src)
-        };
-        if fs::metadata(&dest).is_err() {
-            println!(" mkdir: {:?}", dest);
-            fs::create_dir_all(&dest)?;
-        }
-
-        for entry in fs::read_dir(working_path)? {
-            let entry = entry?;
-            let path = entry.path();
-
-            println!(" found path: {:?}", path);
-
-            if path.is_dir() {
-                stack.push(path);
-            } else {
-                match path.file_name() {
-                    Some(filename) => {
-                        let dest_path = dest.join(filename);
-                        println!("  copy: {:?} -> {:?}", &path, &dest_path);
-                        fs::copy(&path, &dest_path)?;
-                    }
-                    None => {
-                        println!("failed: {:?}", path);
-                    }
-                }
-            }
-        }
+    let out_dir_path = env::var("OUT_DIR").unwrap();
+    let target = Path::new(&out_dir_path).join("../../..");
+    if fs::metadata(&target).is_err() {
+        println!(" mkdir: {:?}", target);
+        fs::create_dir_all(&target).unwrap();
     }
 
-    Ok(())
+    for entry in WalkDir::new(&TEMPLATES_DIR)
+            .into_iter()
+            .filter_map(Result::ok) {
+        
+        let dest_path = target.join(entry.path());
+        if entry.file_type().is_dir() {
+            if fs::metadata(&dest_path).is_err() {
+                println!(" mkdir: {:?}", dest_path);
+                fs::create_dir_all(&dest_path).unwrap();
+            }
+        } else {
+            println!("  copy: {:?} -> {:?}", &entry.path(), &dest_path);
+            fs::copy(&entry.path(), &dest_path).unwrap();
+        }
+    }
 }
