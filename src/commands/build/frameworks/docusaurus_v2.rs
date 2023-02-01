edff9d5ba9f02073c404eb60b8f7bf5f1c744fa0
seancarroll <seanc28@gmail.com>
2023-01-31 23:37:12 -0600

    rename docusaurus to docusaurus_v2 but will probably also need v1

---
+++ src/commands/build/frameworks/docusaurus_v2.rs
@@ -0,0 +1,83 @@
+// docusaurus.config.js
+// npm run build / docusaurus build
+// build directory
+// Both build/serve commands take an output dir option, and there's even a --build option on the serve command. We don't plan to add output dir to the config sorry
+
+
+// docusaurus v1
+// docusaurus-start
+// website/siteConfig.js
+// publish directory -> website/build/<projectName>
+// where projectName is the value you defined in your siteConfig.js
+
+// vercel just sees if there is a single file (directory) and uses it
+// Code
+// If there is only one file in it that is a dir we'll use it as dist dir
+// if (content.length === 1 && content[0].isDirectory()) {
+// return join(base, content[0].name);
+// }
+
+
+// docusaurus v2
+// docusaurus build --out-dir
+// docusaurus.config.js - doesnt contain output
+// defaults to build
+
+
+// TODO: support monorepo
+
+use serde::{Serialize, Deserialize, de};
+use swc_ecma_ast::{ArrayLit, Lit, ModuleDecl, ModuleItem, ObjectLit, Program, Stmt};
+use swc_ecma_ast::Expr::{Array, Object, Tpl};
+use swc_ecma_ast::Stmt::{Decl, Expr};
+
+use crate::commands::build::frameworks::framework::{ConfigurationFileDeserialization, FrameworkInfo, FrameworkSupport, read_config_files};
+use crate::doctavious_error::{DoctaviousError, Result as DoctaviousResult};
+
+// TODO: given there is no option to override does it make sense to still enforce Deserialize
+// and ConfigurationFileDeserialization?
+// I suppose we can determine if gatsby-plugin-output is in the plugins and grab it from there
+#[derive(Deserialize)]
+struct DocusaurusV2Config { output: String }
+
+pub struct DocusaurusV2 { info: FrameworkInfo }
+impl FrameworkSupport for DocusaurusV2 {
+    fn get_info(&self) -> &FrameworkInfo {
+        &self.info
+    }
+
+    fn get_output_dir(&self) -> String {
+        // doesnt support overriding via configuration file
+        // TODO: look at package.json scripts build
+
+        // if (content.length === 1 && content[0].isDirectory()) {
+        // return join(base, content[0].name);
+        // }
+
+        //self.info.build.outputdir
+        String::default()
+    }
+}
+
+impl ConfigurationFileDeserialization for DocusaurusV2Config {}
+
+#[cfg(test)]
+mod tests {
+    use crate::commands::build::frameworks::framework::{FrameworkInfo, FrameworkSupport};
+    use super::DocusaurusV2;
+
+    #[test]
+    fn test_docusaurus() {
+        let docusaurus = DocusaurusV2 {
+            info: FrameworkInfo {
+                name: "".to_string(),
+                website: None,
+                configs: Some(vec![String::from("tests/resources/framework_configs/docusaurus2/docusaurus.config.js")]),
+                project_file: None,
+            }
+        };
+
+        let output = docusaurus.get_output_dir();
+        assert_eq!(output, "build")
+    }
+}
